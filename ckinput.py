27fb13128377a1f7dc62edc3f37b02a2c430cf57
JiHye.Song <48706964+qwer15948@users.noreply.github.com>
2023-05-01 20:07:39 +0900

    feat: 입력값 반환

--- ckinput.py
+++ ckinput.py
@@ -9,5 +9,5 @@ def ckinput():
         if not len(set(exValue)) == 4:
             print("중복된 숫자가 있습니다. 다시 입력해주세요.")
             continue
-            
-    return exValue
+        
+        return exValue
