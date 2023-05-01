6b8ee245b9d6459088440fbd25ab7c703b372cb5
Donghyeok Heo <missko30@naver.com>
2023-05-01 20:17:14 +0900

    feat: Determine whether the value is valid.
    Determine wheter the value form user is valid

--- ckend.py
+++ ckend.py
@@ -1,4 +1,10 @@
 def ckend():
     while True:
         ckg = input('게임종료(0), 시작(1): ')
+        if ckg == '0':
+            break
+        elif ckg == '1':
+            break
+        else:
+            print('잘못된 형식입니다.')
     return ckg
