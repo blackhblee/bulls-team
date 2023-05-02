01212c0730c81abbc12e97c240abe05cfe10b830
blackhblee <blackhblee@gmail.com>
2023-05-02 15:18:46 +0900

    refactor: Refactor ckinput.py
    change korean to english

--- ckinput.py
+++ ckinput.py
@@ -1,13 +1,13 @@
 def ckinput():
     while True:
-        exValue = input('4자리 숫자를 입력하세요: ')
+        exValue = input('Input 4 digit number: ')
         
         if not len(exValue) == 4:
-            print("형식에 맞지 않는 숫자입니다. 다시 입력해주세요.")
+            print("Wrong type of number. Try again.")
             continue
         
         if not len(set(exValue)) == 4:
-            print("중복된 숫자가 있습니다. 다시 입력해주세요.")
+            print("Duplicate. Try again.")
             continue
         
         return exValue
